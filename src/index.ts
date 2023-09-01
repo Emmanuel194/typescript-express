370ea84c3ebdc7e5fb42aa6f37e9664b02eaf043
emmanuel.lima <86535005+Emmanuel194@users.noreply.github.com>
2023-08-31 21:10:09 -0300

    commit atualização

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import express, { Request, Response } from "express";
+
+const app = express();
+
+app.post("/", lidarComRequisicao)
+
+
+function lidarComRequisicao(req: Request, res: Response) {
+  res.status(401).send("Ola, mundo!");
+}
+
+// const aplicacaoIniciando = () => {  // ARROW FUCNTION , modelo dela.
+//   console.log("aplicação Iniciada");
+//  }
+
+app.listen(3000, () => {
+  console.log("servindo rodando na porta 3000")
+});
+
+// console.log("Hello world");
